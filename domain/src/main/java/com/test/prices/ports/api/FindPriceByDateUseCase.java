f0317c8bf72658d8817341756628e44b4673813d
sebaskcc <sebas@T495.local>
2024-10-20 11:18:10 +0200

    fix: complete domain interfaces

--- domain/src/main/java/com/test/prices/ports/api/FindPriceByDateUseCase.java
+++ domain/src/main/java/com/test/prices/ports/api/FindPriceByDateUseCase.java
@@ -2,9 +2,10 @@ package com.test.prices.ports.api;
 
 import com.test.prices.exception.PriceNotFound;
 import com.test.prices.model.Price;
+import java.time.Instant;
 
 public interface FindPriceByDateUseCase {
 
-  Price findPriceByDate() throws PriceNotFound;
+  Price findPriceByDate(Instant applyDate, Long productId, Integer brandId) throws PriceNotFound;
 
 }
--- domain/src/main/java/com/test/prices/ports/spi/PriceRepository.java
+++ domain/src/main/java/com/test/prices/ports/spi/PriceRepository.java
@@ -1,5 +1,11 @@
 package com.test.prices.ports.spi;
 
+import com.test.prices.model.Price;
+import java.time.Instant;
+import java.util.List;
+
 public interface PriceRepository {
+
+  List<Price> findPriceByDate(Instant applyDate, Long productId, Integer brandId);
 
 }
